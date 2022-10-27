eaa301299c2c048402a51186497936326aa3553e
NikishaBongale <nikishabongale2021@gmail.com>
2022-10-27 22:27:39 +0530

    first commit

---
+++ IoT_webProject/js/events.js
@@ -0,0 +1,55 @@
+function dht() {
+  var myWindow = window.open("dhtsensor.html", "MsgWindow", "width=1200,height=900");
+}
+function ultrasonic()
+{
+	var myWindow = window.open("ultrasonicsensor.html", "MsgWindow", "width=1200,height=900");
+}
+function infrared()
+{
+	var myWindow = window.open("infraredsensor.html", "MsgWindow", "width=1200,height=900");
+}
+function ldr()
+{
+	var myWindow = window.open("ldr.html", "MsgWindow", "width=1200,height=900"); 
+}
+function hc05()
+{
+	var myWindow = window.open("hc05.html", "MsgWindow", "width=1200,height=900"); 
+}
+function servomotor()
+{
+	var myWindow = window.open("servomotor.html", "MsgWindow", "width=1200,height=900"); 
+}
+function led()
+{
+	var myWindow = window.open("led.html", "MsgWindow", "width=1200,height=900"); 
+}
+function pushButton()
+{
+	var myWindow = window.open("pushButton.html", "MsgWindow", "width=1200,height=900"); 
+}
+function buzzor()
+{
+	var myWindow = window.open("buzzor.html", "MsgWindow", "width=1200,height=900"); 
+}
+function potentiometer()
+{
+	var myWindow = window.open("potentiometer.html", "MsgWindow", "width=1200,height=900"); 
+}
+function oled()
+{
+	var myWindow = window.open("oled.html", "MsgWindow", "width=1200,height=900"); 
+}
+function arduino()
+{
+	var myWindow = window.open("arduino.html", "MsgWindow", "width=1200,height=900"); 
+}
+function raspberry()
+{
+	var myWindow = window.open("raspberry.html", "MsgWindow", "width=1200,height=900"); 
+}
+function nodemcu()
+{
+	var myWindow = window.open("nodemcu.html", "MsgWindow", "width=1200,height=900"); 
+}
